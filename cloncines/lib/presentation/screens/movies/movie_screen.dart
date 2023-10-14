534292fc8dd9a4e562a193dbe739218f30981aa1
Gerardo Ramírez <gedrix.rm93@gmail.com>
2023-10-14 17:51:20 -0500

    ,

--- cloncines/lib/presentation/screens/movies/movie_screen.dart
+++ cloncines/lib/presentation/screens/movies/movie_screen.dart
@@ -42,9 +42,159 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
       );
     }
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Movie: ${widget.movieId}'),
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: [
+          _CustomSliverAppbBar(movie: movie),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+                  (context, index) => _MovieDetails(movie: movie),
+                  childCount: 1
+                )
+            )
+        ],
       ),
     );
   }
 }
+
+class _MovieDetails extends StatelessWidget {
+  
+  final Movie movie;
+  const _MovieDetails({
+      super.key, 
+      required this.movie
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final  size = MediaQuery.of(context).size;
+    final textStyle = Theme.of(context).textTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20), 
+                  child: Image.network(
+                      movie.posterPath,
+                      width:  size.width * 0.3,
+                    )
+                  ),
+                  //todo integrar votacion y otros datos para complementar
+                  const SizedBox(width: 10,),
+
+                  //description de la pelicula
+                  SizedBox(
+                    width: (size.width - 40) * 0.7,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(movie.title, style: textStyle.titleLarge,),
+                        Text(movie.overview ),
+
+                      ],
+                    ),
+                  ),
+
+              ],
+            ),
+          ),
+
+          //generos de la pelicula
+
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Wrap(
+              children: [
+                ...movie.genreIds.map((gender) => Container(
+                  margin: const EdgeInsets.only(right: 10),
+                  child: Chip(
+                    label: Text(gender),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                  ),
+                ))
+              ],  
+            ),
+          ),
+
+        //TODO MOSOTRAR ACTORES LISTVIEW
+        const SizedBox(height: 100,),
+
+      ],
+    );
+  }
+}
+
+class _CustomSliverAppbBar extends StatelessWidget {
+
+  
+  final Movie movie;
+  const _CustomSliverAppbBar({
+      super.key, required this.movie
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      expandedHeight: size.height * 0.7,
+      foregroundColor: Colors.white,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        title: Text(movie.title, style:  const TextStyle(fontSize: 20), textAlign: TextAlign.start,),
+        background: Stack(
+          children: [
+            SizedBox.expand(
+              child: Image.network(
+                  movie.posterPath,
+                  fit: BoxFit.cover,
+                ),
+            ),
+
+            const SizedBox.expand(
+              child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: [0.7, 1.0],
+                        colors: [
+                          Colors.transparent,
+                          Colors.black87
+                        ]
+                      )
+                  )
+                ),
+            ),
+
+            const SizedBox.expand(
+              child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        stops: [0.0, 0.4],
+                        colors: [
+                          Colors.black87,
+                          Colors.transparent
+                        ]
+                      )
+                  )
+                ),
+            )
+          ],
+        ),
+      
+      ),
+    );
+  }
+}
+
+
